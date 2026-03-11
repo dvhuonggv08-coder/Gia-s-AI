7afa83f09e379893be7e2acada8123a23ab2e9e4
dvhuonggv08-coder <dvhuong.gv08@tuyenquang.edu.vn>
2026-03-11 09:21:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if(userText.trim()=="") return;
 chatBox.innerHTML += "<p><b>Bạn:</b> "+userText+"</p>";
 
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${AIzaSyAFErA40r1QxWc1KIduoC19t3aLcD2DIf0}`,
+`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
 {
 method:"POST",
 headers:{
